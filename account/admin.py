cdc871bd9a819ce38d90d2771ed8285ff48fd35f
itsaisabelle <isabelle.d.campeau@gmail.com>
2026-02-11 15:55:23 -0500

    Set up admin exports for accounts

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import jobsAppliedTo, jobSeeker, recruiter
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
+
+admin.site.register(jobsAppliedTo, ImportExportModelAdmin)
+admin.site.register(jobSeeker, ImportExportModelAdmin)
+admin.site.register(recruiter, ImportExportModelAdmin)

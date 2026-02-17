b337ee6b00ce8aaea9320c904915ed0a689e524c
Christopher Leonard <christophergleonardjr@gmail.com>
2026-02-17 01:48:14 -0500

    Working functionality for recruiter candidate search filters

--- applicants/views.py
+++ applicants/views.py
@@ -7,11 +7,24 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Applicants'
-    template_data['applicants'] = jobSeeker.objects.all()
-
     if (not request.user.is_recruiter):
         return redirect('home.index')
+    
+    template_data = {}
+    template_data['title'] = 'Applicants'
+    applicantList = jobSeeker.objects.all()
+
+    projects = request.GET.get('projects')
+    location = request.GET.get('location')
+    skill = request.GET.get('skill')
+    
+    if projects:
+        applicantList = applicantList.filter(work_experience__icontains=projects)
+    if location:
+        applicantList = applicantList.filter(currentLocation__icontains=location)
+    if skill:
+        applicantList = applicantList.filter(skills__icontains=skill)
+
+    template_data['applicants'] = applicantList
 
     return render(request, 'applicants/index.html', {'template_data': template_data})

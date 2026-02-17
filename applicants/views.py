b4f9d48421c250a44cc6925b347ffa421373cc46
Christopher Leonard <christophergleonardjr@gmail.com>
2026-02-17 00:48:41 -0500

    Created CSS for applicants page

--- applicants/views.py
+++ applicants/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
 from account.models import jobSeeker
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     template_data = {}
     template_data['title'] = 'Applicants'
     template_data['applicants'] = jobSeeker.objects.all()
+
+    if (not request.user.is_recruiter):
+        return redirect('home.index')
+
     return render(request, 'applicants/index.html', {'template_data': template_data})
